dcf4e4ea0ed80eddaa79f9c7a92f8a5f99d90500
Sameer <sk6586418@gmail.com>
2026-05-12 08:00:37 +0530

    feat: add useAuth hook for authentication handling with register, login, getMe, and logout functions

---
+++ Frontend/src/features/auth/hooks/useAuth.js
@@ -0,0 +1,68 @@
+import { useDispatch } from "react-redux";
+import { registerApi, loginApi, getMeApi, logoutApi } from "../services/auth.api";
+import { setUser, setLoading, setError } from "../auth.slice";
+
+export const useAuth = () => {
+
+    const dispatch = useDispatch()
+
+    const handleRegister = async ({username, email, password}) => {
+        try {
+            dispatch(setLoading(true))
+            const response = await registerApi(username, email, password)
+            const user = response?.data?.user;
+            if (user) dispatch(setUser(user));
+            return { success: true, user: user };
+        } catch (err) {
+            dispatch(setError(err.response?.data?.message || "Registration failed"))
+            return { success: false, error: err }
+        } finally {
+            dispatch(setLoading(false))
+        }
+    }
+
+    const handleLogin = async ({email, password}) => {
+        dispatch(setLoading(true));
+        try {
+            const response = await loginApi(email, password);
+            const user = response?.data?.user;
+            if (user) dispatch(setUser(user));
+            return { success: true, user: user }
+        } catch (err) {
+            return { success: false, error: err }
+        } finally {
+            dispatch(setLoading(false))
+        }
+    }
+
+    const handleGetMe = async () => {
+        try {
+            dispatch(setLoading(true))
+            const response = await getMeApi();
+            const user = response?.data?.user;
+            if (user) dispatch(setUser(user));
+            return user ?? null
+        } catch {
+            dispatch(setUser(null))
+            return null
+        } finally {
+            dispatch(setLoading(false))
+        }
+    }
+
+    const handleLogout = async () => {
+        try {
+            await logoutApi()
+            dispatch(setUser(null))
+        } catch (err) {
+            console.error('Logout failed:', err)
+        }
+    }
+
+    return {
+        handleLogin,
+        handleRegister,
+        handleGetMe,
+        handleLogout,
+    }
+}
