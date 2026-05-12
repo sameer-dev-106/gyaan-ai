5066323d14c7c74e6e92be944f1ee17a2685dc07
Sameer <sk6586418@gmail.com>
2026-05-12 07:55:55 +0530

    Set up middleware for logging and CORS, and integrate authentication routes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,5 +1,10 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+import morgan from "morgan";
+
+import authRouter from "./routes/auth.routes.js";
+import handleError from "./middlewares/error.middleware.js";
 
 const app = express();
 
@@ -7,10 +12,21 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(morgan("dev"));
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"]
+}));
 
 // Health check route
 app.get("/", (req, res) => {
     res.json({message: "server is running"});
 });
+
+app.use("/api/auth", authRouter);
+
+// Error handling middleware
+app.use(handleError);
 
 export default app;
