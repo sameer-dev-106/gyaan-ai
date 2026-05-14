79d1143db0a5d79d8b8857d817753e8f3c447441
Sameer <sk6586418@gmail.com>
2026-05-14 08:35:18 +0530

    feat: refactor server setup to use httpServer for improved socket integration

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,17 @@
 import app from "./src/app.js";
 import connectDB from "./src/config/database.js";
 
+import http from "http";
+import { initSocket } from "./src/socket/server.socket.js";
+
 const PORT = process.env.PORT || 3000;
+
+const httpServer = http.createServer(app);
+
+initSocket(httpServer);
 
 connectDB();
 
-app.listen(PORT,()=>{
+httpServer.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`)
 });
