e7775f924ce1b06fb9e33dd50623bb0240ab4acb
Sameer <sk6586418@gmail.com>
2026-05-14 08:36:19 +0530

    feat: implement sendMessage function for chat message handling

---
+++ Backend/src/controllers/chat.controller.js
@@ -0,0 +1,44 @@
+import { generateChatTitle, generateResponse } from "../services/ai.service.js";
+import chatModel from "../models/chat.model.js";
+import messageModel from "../models/message.model.js";
+
+export async function sendMessage(req, res, next) {
+    try {
+        const { message, chat: chatId } = req.body;
+
+        let title = null, chat = null;
+
+        if (!chatId) {
+            title = await generateChatTitle(message);
+            chat = await chatModel.create({
+                user: req.user.id,
+                title
+            });
+        }
+
+        const userMessage = await messageModel.create({
+            chat: chatId || chat._id,
+            content: message,
+            role: user,
+        });
+
+        const messages = await messageModel.find({ chat: chatId });
+
+        const result = await generateResponse(messages);
+
+        const aiMessage = await messageModel.create({
+            chat: chatId || chat._id,
+            content: result,
+            role: ai,
+        });
+
+        res.status(201).json({
+            title,
+            chat,
+            aiMessage
+        })
+
+    } catch (err) {
+        next(err);
+    }
+}
