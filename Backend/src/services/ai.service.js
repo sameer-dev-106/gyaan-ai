426685ab3f27c39af2368b5a891c24ebd24cd5f7
Sameer <sk6586418@gmail.com>
2026-05-21 22:36:02 +0530

    feat: integrate searchInternet tool and refactor response generation for enhanced AI capabilities

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -1,7 +1,9 @@
+import { config } from "../config/config.js";
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 import { ChatMistralAI } from "@langchain/mistralai";
-import { HumanMessage, SystemMessage, AIMessage } from "langchain";
-import { config } from "../config/config.js";
+import { HumanMessage, SystemMessage, AIMessage, tool, createAgent } from "langchain";
+import { searchInternet } from "./internet.service.js";
+import * as z from "zod";
 
 const geminiModel = new ChatGoogleGenerativeAI({
     model: "gemini-2.5-flash-lite",
@@ -13,19 +15,44 @@ const mistralModel = new ChatMistralAI({
     apiKey: config.MISTRAL_API_KEY
 });
 
+const searchInternetTool = tool(
+    searchInternet,
+    {
+        name: "searchInternet",
+        description: "Use this tool to get latest information from the internet.",
+        schema: z.object({
+            query: z.string().describe("The search query to look up on the internet.")
+        })
+    }
+);
+
+const agent = createAgent({ model: mistralModel, tools: [searchInternetTool] });
+
 export const generateResponse = async (messages) => {
+    console.log(messages)
     try {
-        const response = await geminiModel.invoke(messages.map(msg => {
-            if (msg.role == "user") {
-                return new HumanMessage(msg.content);
-            } else if (msg.role == "ai") {
-                return new AIMessage(msg.content);
-            }
-        }));
-        return response.text;
-    } catch (error) {
-        console.error("Error generating Gemini AI response:", error);
-        // throw new Error("Failed to generate Gemini AI response");
+        const response = await agent.invoke({
+            messages: [
+                new SystemMessage(`
+                    You are a helpful and precise assistant for answering questions.
+                    If you don't know the answer, say you don't know. 
+                    If the question requires up-to-date information, use the "searchInternet" tool to get the latest information from the internet and then answer based on the search results.
+                `),
+                ...(messages.map(msg => {
+                    if (msg.role == "user") {
+                        return new HumanMessage(msg.content);
+                    } else if (msg.role == "ai") {
+                        return new AIMessage(msg.content);
+                    }
+                }))
+            ]
+        });
+        console.log(response.messages[response.messages.length - 1].text);
+        return response.messages[response.messages.length - 1].text;
+    } catch (err) {
+        console.log(messages)
+        console.error("Error generating Gemini AI response:", err);
+        throw new Error("Failed to generate Gemini AI response" + err.message);
     }
 }
 
@@ -42,9 +69,7 @@ export const generateChatTitle = async (message) => {
                 "${message}"
             `)
         ]);
-
         return response.text;
-
     } catch (error) {
         console.error("Error generating Mistral AI response:", error);
         throw new Error("Failed to generate Mistral AI response");
